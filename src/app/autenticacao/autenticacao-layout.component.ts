439fd7792b4eaf647e0c06d01b7b5f0f56eb7c74
GeovaniSS <geosilvasousa@gmail.com>
2025-06-07 18:10:52 -0300

    feat: adicionado Autenticacao Layout

---
+++ src/app/autenticacao/autenticacao-layout.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './autenticacao-layout.component.html',
+  standalone: false,
+})
+export class AutenticacaoLayoutComponent {}
