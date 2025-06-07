fa7abade3ab01d171dc377de4185c71483aa63af
GeovaniSS <geosilvasousa@gmail.com>
2025-06-07 18:08:23 -0300

    feat: adicionado traducao do primeNG

--- src/core/app-preset.ts
+++
@@ -1,6 +0,0 @@
-import { definePreset } from '@primeng/themes';
-import Aura from '@primeng/themes/aura';
-
-export const AppPreset = definePreset(Aura, {
-  //Your customizations, see the following sections for examples
-});
