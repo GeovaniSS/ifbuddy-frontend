0f2c8dad9fc9691c3fa46cf2175a3cb59afa79ee
GeovaniSS <geosilvasousa@gmail.com>
2025-06-07 18:05:30 -0300

    feat: adicionado EnderecoService

---
+++ src/app/estudante/edicao-perfil/services/endereco.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { environment } from '../../../../environments/environment';
+import { Cidade } from '../../../core/entities/ibge-cidades.entities';
+import { Estado } from '../../../core/entities/ibge-estados.entities';
+
+@Injectable()
+export class EnderecoService {
+  constructor(private readonly http: HttpClient) {}
+
+  listarEstados() {
+    return this.http.get<Estado[]>(
+      `${environment.ibgeUrl}/localidades/estados`,
+    );
+  }
+
+  listarCidadesPorEstado(estadoId: number) {
+    return this.http.get<Cidade[]>(
+      `${environment.ibgeUrl}/localidades/estados/${estadoId}/subdistritos`,
+    );
+  }
+}
